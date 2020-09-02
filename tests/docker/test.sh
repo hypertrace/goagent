87f357bb053537e2e2d8807ad8ed8fcb3a623cd7
José Carlos Chávez <jcchavezs@gmail.com>
2020-09-02 06:01:09 +0200

    tests(container_id): improves the test.sh feedback to the user.

--- tests/docker/test.sh
+++ tests/docker/test.sh
@@ -11,16 +11,21 @@ docker build --no-cache -f tests/docker/Dockerfile -t goagent-test .
 # docker file
 ACTUAL_CONTAINER_ID=$(docker run --name goagent-test goagent-test)
 
+if [[ -z "$ACTUAL_CONTAINER_ID" ]]; then
+    echo "Failed to obtain the actual container ID, empty value"
+    exit 1
+fi
+
 # EXPECTED_SHORTEN_CONTAINER_ID is obtained from `docker ps`
 EXPECTED_SHORTEN_CONTAINER_ID=$(docker ps -q -a --filter "name=goagent-test")
 
-SHORTEN_LENGTH=${#EXPECTED_SHORTEN_CONTAINER_ID} # the length of the short format
-ACTUAL_SHORTEN_CONTAINER_ID="${ACTUAL_CONTAINER_ID:0:$SHORTEN_LENGTH}"
-
 if [[ -z "$EXPECTED_SHORTEN_CONTAINER_ID" ]]; then
-    echo "Failed to obtain the container ID, empty value"
+    echo "Failed to obtain the expected container ID, empty value"
     exit 1
 fi
+
+SHORTEN_LENGTH=${#EXPECTED_SHORTEN_CONTAINER_ID} # the length of the short format
+ACTUAL_SHORTEN_CONTAINER_ID="${ACTUAL_CONTAINER_ID:0:$SHORTEN_LENGTH}"
 
 echo ""
 if [[ "$EXPECTED_SHORTEN_CONTAINER_ID" == "$ACTUAL_SHORTEN_CONTAINER_ID" ]]; then

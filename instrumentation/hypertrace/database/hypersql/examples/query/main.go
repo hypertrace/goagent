a918fc9bd2c0b89b6c2ad4140d4cb9c197a6776f
José Carlos Chávez <jcchavezs@gmail.com>
2020-11-23 14:52:55 +0100

    chore(sql): adds examples.

---
+++ instrumentation/hypertrace/database/hypersql/examples/query/main.go
@@ -0,0 +1,65 @@
+// +build ignore
+
+package main
+
+import (
+	"context"
+	"database/sql"
+	"database/sql/driver"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/hypertrace/goagent/config"
+	"github.com/hypertrace/goagent/instrumentation/hypertrace"
+	"github.com/hypertrace/goagent/instrumentation/hypertrace/database/hypersql"
+)
+
+// Run docker run mysql
+func main() {
+	cfg := config.Load()
+	cfg.ServiceName = config.String("querier")
+
+	flusher := hypertrace.Init(cfg)
+	defer flusher()
+
+	var (
+		driver driver.Driver
+		db     *sql.DB
+	)
+
+	// Explicitly wrap the MySQLDriver driver with hypersql.
+	driver = hypersql.Wrap(&mysql.MySQLDriver{})
+
+	// Register our hypersql wrapper as a database driver.
+	sql.Register("ht-mysql", driver)
+
+	// Connect to a mysql database using the hypersql driver wrapper.
+	// ?interpolateParams=true will escape the variables for any requests
+	// and send ready-for-use queries to the server for github.com/go-sql-driver/mysql.
+	// This save us a meaningless span.
+	db, err := sql.Open("ht-mysql", "root:root@tcp(localhost)/?interpolateParams=true")
+	if err != nil {
+		log.Fatalf("failed to connect the DB: %v", err)
+	}
+
+	const dbPingRetries = 5
+	for i := 0; i <= dbPingRetries; i++ {
+		if err := db.Ping(); err != nil && i == dbPingRetries {
+			log.Fatalf("failed to ping the DB: %v", err)
+		}
+		time.Sleep(time.Second)
+	}
+
+	rows, err := db.QueryContext(context.Background(), "SELECT 'Hi there :)'")
+	if err != nil {
+		log.Fatalf("failed to retrieve message: %v", err)
+	}
+
+	for rows.Next() {
+		m := new(string)
+		rows.Scan(m)
+		fmt.Println(*m)
+	}
+}

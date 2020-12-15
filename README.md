e23a872531084fa4e23432916f8f4bd0cb9939ad
José Carlos Chávez <jcchavezs@gmail.com>
2020-12-15 14:18:25 +0100

    chore: adds release process. (#69)
    * chore: adds release process.
    
    * chore: adds a check for 0.0.0.
    
    * docs: adds to-do.
    
    * fix: fixes the resource attributes declaration for SDK.
    
    * docs: improves documentation for release process.

--- README.md
+++ README.md
@@ -169,3 +169,7 @@ for unit tests only
 ```bash
 make test-unit
 ```
+
+### Releasing
+
+Run `./release.sh <version_number>` (`<version_number>` should follow semver, e.g. `1.2.3`). The script will change the hardcoded version, commit it, push a tag and prepare the hardcoded version for the next release.
--- instrumentation/hypertrace/init.go
+++ instrumentation/hypertrace/init.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/hypertrace/goagent/config"
 	sdkconfig "github.com/hypertrace/goagent/sdk/config"
+	"github.com/hypertrace/goagent/version"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/trace/zipkin"
 	"go.opentelemetry.io/otel/propagation"
@@ -39,8 +40,10 @@ func Init(cfg *config.AgentConfig) func() {
 	}
 
 	resources, err := resource.New(context.Background(), resource.WithAttributes(
-		semconv.ServiceNameKey.String(cfg.GetServiceName().GetValue())),
-	)
+		semconv.ServiceNameKey.String(cfg.GetServiceName().GetValue()),
+		semconv.TelemetrySDKNameKey.String("hypertrace"),
+		semconv.TelemetrySDKVersionKey.String(version.Version),
+	))
 	if err != nil {
 		log.Fatal(err)
 	}

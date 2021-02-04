1d73f8d0404fb6da47e3f08f73e5f03b0fb356f1
Ryan Burn <ryan.burn@gmail.com>
2021-02-03 16:47:58 -0800

    Support different propagation formats

--- instrumentation/opentelemetry/init_test.go
+++ instrumentation/opentelemetry/init_test.go
@@ -1,6 +1,13 @@
 package opentelemetry
 
-import "github.com/hypertrace/goagent/config"
+import (
+	"context"
+	"testing"
+
+	"github.com/hypertrace/goagent/config"
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/otel"
+)
 
 func ExampleInit() {
 	cfg := config.Load()
@@ -10,4 +17,32 @@ func ExampleInit() {
 
 	shutdown := Init(cfg)
 	defer shutdown()
+}
+
+type carrier struct {
+	m map[string]string
+}
+
+func (c carrier) Get(string) string { return "" }
+
+func (c carrier) Set(key string, value string) {
+	c.m[key] = value
+}
+
+func TestPropagationFormats(t *testing.T) {
+	cfg := config.Load()
+	cfg.PropagationFormats = []config.PropagationFormat{
+		config.PropagationFormat_B3,
+		config.PropagationFormat_TRACECONTEXT,
+	}
+	Init(cfg)
+	tracer := otel.Tracer("b3")
+	ctx, _ := tracer.Start(context.Background(), "test")
+	propagator := otel.GetTextMapPropagator()
+	c := carrier{make(map[string]string)}
+	propagator.Inject(ctx, c)
+	_, ok := c.m["x-b3-traceid"]
+	assert.True(t, ok)
+	_, ok = c.m["traceparent"]
+	assert.True(t, ok)
 }

ce39c929063eb7e3aad10487b4bee75f1dfac14a
José Carlos Chávez <jcchavezs@gmail.com>
2021-02-21 17:12:14 +0100

    chore(hypertrace): reuse opentelemetry Init in hypertrace package.

--- instrumentation/hypertrace/init.go
+++ instrumentation/hypertrace/init.go
@@ -1,72 +1,9 @@
 package hypertrace
 
 import (
-	"context"
-	"crypto/tls"
-	"log"
-	"net/http"
-	"time"
-
-	"github.com/hypertrace/goagent/config"
-	sdkconfig "github.com/hypertrace/goagent/sdk/config"
-	"github.com/hypertrace/goagent/version"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/trace/zipkin"
-	"go.opentelemetry.io/otel/propagation"
-	"go.opentelemetry.io/otel/sdk/resource"
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	"go.opentelemetry.io/otel/semconv"
+	"github.com/hypertrace/goagent/instrumentation/opentelemetry"
 )
 
-const batchTimeoutInSecs = 200.0
-
-// Init initializes opentelemetry tracing and returns a shutdown function to flush data immediately
+// Init initializes hypertrace tracing and returns a shutdown function to flush data immediately
 // on a termination signal.
-func Init(cfg *config.AgentConfig) func() {
-	sdkconfig.InitConfig(cfg)
-
-	client := &http.Client{Transport: &http.Transport{
-		TLSClientConfig: &tls.Config{
-			InsecureSkipVerify: !cfg.GetReporting().GetSecure().GetValue()},
-	}}
-
-	zipkinBatchExporter, err := zipkin.NewRawExporter(
-		cfg.GetReporting().GetEndpoint().GetValue(),
-		cfg.GetServiceName().GetValue(),
-		zipkin.WithClient(client),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	resources, err := resource.New(context.Background(), resource.WithAttributes(
-		semconv.ServiceNameKey.String(cfg.GetServiceName().GetValue()),
-		semconv.TelemetrySDKNameKey.String("hypertrace"),
-		semconv.TelemetrySDKVersionKey.String(version.Version),
-	))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	tp := sdktrace.NewTracerProvider(
-		sdktrace.WithConfig(sdktrace.Config{DefaultSampler: sdktrace.AlwaysSample()}),
-		sdktrace.WithBatcher(zipkinBatchExporter, sdktrace.WithBatchTimeout(batchTimeoutInSecs*time.Millisecond)),
-		sdktrace.WithResource(resources),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	otel.SetTracerProvider(tp)
-
-	// TODO: support configurable propagation
-	otel.SetTextMapPropagator(propagation.TraceContext{})
-
-	// TODO: use batcher instead of this hack:
-	return func() {
-		// This is a sad temporary solution for the lack of flusher in the batcher interface.
-		// What we do here is that we wait for `batchTimeout` seconds as that is the time configured
-		// in the batcher and hence we make sure spans had time to be flushed.
-		// In next versions the flush functionality is finally added and we will use it.
-		<-time.After(batchTimeoutInSecs * 1.5 * time.Millisecond)
-	}
-}
+var Init = opentelemetry.Init

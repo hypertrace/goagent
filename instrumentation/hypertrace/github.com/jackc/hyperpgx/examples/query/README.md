87813da68db49a5b5756bc98af26025e8b118a2f
José Carlos Chávez <jcchavezs@gmail.com>
2021-04-04 22:20:54 +0200

    chore(pgx): clarifies the example run.

---
+++ instrumentation/hypertrace/github.com/jackc/hyperpgx/examples/query/README.md
@@ -0,0 +1,7 @@
+# hyperPGX query example
+
+Before running the `main.go` file make sure you have a PostgreSQL database up and running with the following command:
+
+```bash
+docker run -e POSTGRES_USER=root -e POSTGRES_PASSWORD=root -p 5432:5432 postgres
+```
--- instrumentation/hypertrace/github.com/jackc/hyperpgx/examples/query/main.go
+++ instrumentation/hypertrace/github.com/jackc/hyperpgx/examples/query/main.go
@@ -8,26 +8,22 @@ import (
 	"os"
 
 	"github.com/hypertrace/goagent/instrumentation/opentelemetry/github.com/jackc/hyperpgx"
-	"github.com/jackc/pgx/v4"
 )
 
 func main() {
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	conn, err := hyperpgx.Connect(context.Background(),"root:root@localhost")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
 	defer conn.Close(context.Background())
 
-	wConn := hyperpgx.WrapConnection(conn)
-
-	var name string
-	var weight int64
-	err = wConn.QueryRow(context.Background(), "select name, weight from widgets where id=$1", 42).Scan(&name, &weight)
+	m := new(string)
+	err = conn.QueryRow(context.Background(), "SELECT 'Hi there :)'").Scan(m)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(name, weight)
+	fmt.Println(*m)
 }

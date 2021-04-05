ab40c70505790c130e20e5ee91242fe3cfeba56f
José Carlos Chávez <jcchavezs@gmail.com>
2021-04-05 23:10:57 +0200

    Adds jackc/pgx instrumentation (#111)
    * chore: adds jackc/hyperpgx instrumentation.
    
    * chore: adds DSN parsing support.
    
    * tests(pgx): adds unit and integration tests for PGX.
    
    * tests(pgx): adds integration test for PGX.
    
    * chore(pgx): clarifies the example run.
    
    * chore(pgx): fix lint and removes unused code.
    
    * docs(pgx): copies method descriptions from the PGX connection into the interface.
    
    * chore: adds clarifications in the code.
    
    * chore(pgx): clarifies the DSN parsing.

---
+++ instrumentation/opentelemetry/github.com/jackc/hyperpgx/dsn.go
@@ -0,0 +1,40 @@
+package hyperpgx
+
+import (
+	"fmt"
+	"net/url"
+)
+
+// parseDSN parses the connection string provided for postgres driver.
+func parseDSN(dsn string) (map[string]string, error) {
+	parsedURL, err := url.Parse(dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse connection %q string: %v", dsn, err)
+	}
+
+	if parsedURL.Scheme != "postgres" {
+		return nil, fmt.Errorf("invalid postgres connection string %q: it should use \"postgres\" as protocol", dsn)
+	}
+
+	connAttrs := map[string]string{}
+
+	if parsedURL.User != nil && parsedURL.User.Username() != "" {
+		connAttrs["db.user"] = parsedURL.User.Username()
+	} else if queryUser := parsedURL.Query().Get("user"); queryUser != "" {
+		connAttrs["db.user"] = queryUser
+	}
+
+	if parsedURL.Hostname() != "" {
+		connAttrs["net.peer.name"] = parsedURL.Hostname()
+	}
+
+	if parsedURL.Port() != "" {
+		connAttrs["net.peer.port"] = parsedURL.Port()
+	}
+
+	if parsedURL.Path != "/" && parsedURL.Path != "" {
+		connAttrs["db.name"] = parsedURL.Path[1:]
+	}
+
+	return connAttrs, nil
+}

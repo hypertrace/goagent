5f855e94c1b8f99795a4e736132e80092577bbb5
prodion23 <donniepropst@donnies-mbp.lan>
2021-07-13 16:23:51 -0400

    Lint

--- sdk/instrumentation/google.golang.org/grpc/serializer_test.go
+++ sdk/instrumentation/google.golang.org/grpc/serializer_test.go
@@ -7,14 +7,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestWithProtobufMessageFormatV2(t *testing.T){
+func TestWithProtobufMessageFormatV2(t *testing.T) {
 	msg := &pb.HelloReply{Message: "Test Message"}
 	data, err := marshalMessageableJSON(msg)
 	assert.Nil(t, err)
 	assert.Equal(t, "{\"message\":\"Test Message\"}", string(data[:]))
 }
 
-func TestWithProtobufMessageFormatV1(t *testing.T){
+func TestWithProtobufMessageFormatV1(t *testing.T) {
 	msg := &pb.HelloResponse{Name: "Test Name"}
 	data, err := marshalMessageableJSON(msg)
 	assert.Nil(t, err)

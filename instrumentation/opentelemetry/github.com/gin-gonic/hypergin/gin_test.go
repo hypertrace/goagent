6215c665a1019b1738a5ad5741e8ed6e85b4e91a
prodion23 <donniepropst@donnies-mbp.lan>
2021-07-16 14:10:10 -0400

    Add test showing request tracing is retained through gin middleware

--- instrumentation/opentelemetry/github.com/gin-gonic/hypergin/gin_test.go
+++ instrumentation/opentelemetry/github.com/gin-gonic/hypergin/gin_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/hypertrace/goagent/instrumentation/hypertrace/net/hyperhttp"
 	"github.com/hypertrace/goagent/instrumentation/opentelemetry/internal"
 	sdkhttp "github.com/hypertrace/goagent/sdk/instrumentation/net/http"
 	"go.opentelemetry.io/otel/trace"
@@ -61,4 +62,53 @@ func TestSpanRecordedCorrectly(t *testing.T) {
 	assert.Equal(t, "xyz123abc", attrs.Get("http.response.header.request_id").AsString())
 	assert.Equal(t, `{"code":200,"id":"123"}`, attrs.Get("http.response.body").AsString())
 	assert.Equal(t, "application/json; charset=utf-8", attrs.Get("http.response.header.content-type").AsString())
+}
+
+func TestTraceContextIsPropagated(t *testing.T) {
+	_, flusher := internal.InitTracer()
+
+	// Configure Gin server
+	r := gin.Default()
+	r.Use(Middleware(&sdkhttp.Options{}))
+	r.POST("/things/:thing_id", handler)
+
+	server := &http.Server{Addr: ":60543", Handler: r}
+	defer server.Close()
+
+	go server.ListenAndServe()
+
+	// Configure http Client
+	client := http.Client{
+		Transport: hyperhttp.NewTransport(
+			http.DefaultTransport,
+		),
+	}
+
+	req, _ := http.NewRequest("POST",
+		"http://localhost:60543/things/123",
+		bytes.NewBufferString(`{"name":"Jacinto"}`))
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	if want, have := 200, res.StatusCode; want != have {
+		t.Errorf("unexpected status code, want %q, have %q", want, have)
+	}
+
+	spans := flusher()
+	if spans == nil {
+		t.Errorf("failed")
+	}
+
+	assert.Equal(t, 1, len(spans))
+
+	span := spans[0]
+	attrs := internal.LookupAttributes(span.Attributes)
+	b3RequestHeader := attrs.Get("http.request.header.b3").AsString()
+	expectedHeader := span.SpanContext.TraceID().String() + "-" + span.ParentSpanID.String() + "-1"
+	assert.Equal(t, b3RequestHeader, expectedHeader)
+
 }

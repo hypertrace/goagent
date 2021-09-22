c2b17d28ce5f1314249743fd8595422621ba8960
José Carlos Chávez <jcchavezs@gmail.com>
2021-09-22 23:13:34 +0200

    chore: follows OTel proto naming.

--- sdk/codes.go
+++ sdk/codes.go
@@ -4,10 +4,10 @@ type Code uint32
 
 const (
 	// Unset is the default status code.
-	Unset Code = 0
+	StatusCodeUnset Code = 0
 	// Error indicates the operation contains an error.
-	Error Code = 1
+	StatusCodeError Code = 1
 	// Ok indicates operation has been validated by an Application developers
 	// or Operator to have completed successfully, or contain no error.
-	Ok Code = 2
+	StatusCodeOk Code = 2
 )

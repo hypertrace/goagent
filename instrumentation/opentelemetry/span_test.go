7772dbf6d99693b87a312209eba93d1fb73fb207
José Carlos Chávez <jcchavezs@gmail.com>
2021-09-27 22:44:19 +0200

    tests: improves coverage for generating attributes.

--- instrumentation/opentelemetry/span_test.go
+++ instrumentation/opentelemetry/span_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hypertrace/goagent/sdk"
 	"github.com/stretchr/testify/assert"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
 )
 
@@ -41,4 +42,21 @@ func TestSetAttributeSuccess(t *testing.T) {
 	s.SetAttribute("test_key_3", float64(1.2))
 	s.SetAttribute("test_key_4", "abc")
 	s.SetAttribute("test_key_4", errors.New("xyz"))
+}
+
+func TestGenerateAttribute(t *testing.T) {
+	assert.Equal(t, attribute.BOOL, generateAttribute("key", true).Value.Type())
+	assert.Equal(t, attribute.BOOLSLICE, generateAttribute("key", []bool{true}).Value.Type())
+	assert.Equal(t, attribute.INT64, generateAttribute("key", 1).Value.Type())
+	assert.Equal(t, attribute.INT64SLICE, generateAttribute("key", []int{1}).Value.Type())
+	assert.Equal(t, attribute.INT64, generateAttribute("key", int64(1)).Value.Type())
+	assert.Equal(t, attribute.INT64SLICE, generateAttribute("key", []int64{1}).Value.Type())
+	assert.Equal(t, attribute.FLOAT64, generateAttribute("key", 1.23).Value.Type())
+	assert.Equal(t, attribute.FLOAT64SLICE, generateAttribute("key", []float64{1.23}).Value.Type())
+	assert.Equal(t, attribute.STRING, generateAttribute("key", "val").Value.Type())
+	assert.Equal(t, attribute.STRINGSLICE, generateAttribute("key", []string{"val"}).Value.Type())
+
+	attr := generateAttribute("key", errors.New("x"))
+	assert.Equal(t, attribute.STRING, attr.Value.Type())
+	assert.Equal(t, "x", attr.Value.AsString())
 }

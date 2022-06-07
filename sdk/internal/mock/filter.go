9a1b16e0a41f27f279ef1932ff410d44b6695b6c
Tyrone Yu <tyrone@traceable.ai>
2022-06-07 15:45:08 -0700

    feat: Add filter interface Evaluate()

--- sdk/internal/mock/filter.go
+++ sdk/internal/mock/filter.go
@@ -5,6 +5,7 @@ import "github.com/hypertrace/goagent/sdk"
 type Filter struct {
 	URLAndHeadersEvaluator func(span sdk.Span, url string, headers map[string][]string) bool
 	BodyEvaluator          func(span sdk.Span, body []byte, headers map[string][]string) bool
+	Evaluator              func(span sdk.Span, url string, body []byte, headers map[string][]string) bool
 }
 
 func (f Filter) EvaluateURLAndHeaders(span sdk.Span, url string, headers map[string][]string) bool {
@@ -13,4 +14,8 @@ func (f Filter) EvaluateURLAndHeaders(span sdk.Span, url string, headers map[str
 
 func (f Filter) EvaluateBody(span sdk.Span, body []byte, headers map[string][]string) bool {
 	return f.BodyEvaluator != nil && f.BodyEvaluator(span, body, headers)
+}
+
+func (f Filter) Evaluate(span sdk.Span, url string, body []byte, headers map[string][]string) bool {
+	return f.Evaluator != nil && f.Evaluator(span, url, body, headers)
 }

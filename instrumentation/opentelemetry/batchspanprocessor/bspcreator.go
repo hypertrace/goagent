e5fedb73001460a2cd2eef812cd9e2954113c282
Tim Mwangi <tim@traceable.ai>
2023-01-30 07:40:44 -0800

    fix lint issue

--- instrumentation/opentelemetry/batchspanprocessor/bspcreator.go
+++ instrumentation/opentelemetry/batchspanprocessor/bspcreator.go
@@ -8,7 +8,6 @@ func CreateBatchSpanProcessor(useModified bool, exporter sdktrace.SpanExporter,
 	options ...sdktrace.BatchSpanProcessorOption) sdktrace.SpanProcessor {
 	if useModified {
 		return NewBatchSpanProcessor(exporter, options...)
-	} else {
-		return sdktrace.NewBatchSpanProcessor(exporter, options...)
 	}
+	return sdktrace.NewBatchSpanProcessor(exporter, options...)
 }

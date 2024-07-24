3b993ab4b4b4c14e7a72a40faa9c57b1d40b77cb
test <test@test-ka-MacBook-Pro.local>
2024-07-24 09:26:19 +0530

    updating filter result

--- sdk/filter/result/filter_result.go
+++ sdk/filter/result/filter_result.go
@@ -1,7 +1,17 @@
 package result
 
+type KeyValueString struct {
+	key   string
+	value string
+}
+
+type Decorations struct {
+	RequestHeaderInjections []KeyValueString
+}
+
 type FilterResult struct {
 	Block              bool
 	ResponseStatusCode int32
 	ResponseMessage    string
+	Decorations        Decorations
 }

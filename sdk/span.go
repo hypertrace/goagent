63e66f46e7b88010ba2f4565094e411ead2e9ade
Raghav Rathi <raghavrathi@Raghavs-MacBook-Pro.local>
2025-02-17 15:21:04 +0530

    Comments

--- sdk/span.go
+++ sdk/span.go
@@ -37,6 +37,7 @@ type Span interface {
 	// AddEvent adds an event to the Span with the provided name, timestamp and attributes.
 	AddEvent(name string, ts time.Time, attributes map[string]interface{})
 
+	//GetSpanId fetches the ID of the span that it is called upon
 	GetSpanId() string
 }
 
